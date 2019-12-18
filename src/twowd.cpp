7b0bb1e27eb74d88c01d77e98c2a85e34f927414
Tiogaplanet <samuel.trassare@gmail.com>
2019-12-18 10:25:21 -0500

    Untested, fix for motor operation.
    Clarified motor pins and comments using documentation from Pololu.  Executed a code walk-through to identify incorrect motor operation.  To-do: test it later.

--- src/twowd.cpp
+++ src/twowd.cpp
@@ -23,10 +23,10 @@
 // Number of milliseconds to wait between retries.
 #define TWOWD_BEGIN_RETRY_WAIT 500
 
+// Hardware settings.
+
 // Baud rate for 2wd serial communications.
 #define TWOWD_BAUD_RATE 9600
-
-// Hardware setup.
 
 // OLED display address on the I2C bus.
 #define OLED_DEVICE_ADDRESS 0x3C
@@ -40,17 +40,19 @@
 // OLED display height, in pixels.
 #define SCREEN_HEIGHT 64
 
-// The left motor is connected to pin 4.
-#define TWOWD_ENABLE_LEFT 4
+// Note: The PHASE and ENABLE comments below come from documentation at https://www.pololu.com/product/2135.
+// Another note: Considering a different motor driver with a higher continuous output current.
+// The PHASE pin determines the motor direction.
+#define TWOWD_PHASE_LEFT 4
 
-// Pin 5 is the pulse-width modulation pin for the left motor.
-#define TWOWD_PULSE_LEFT 5
+// The ENABLE pin can be supplied with a PWM signal to control the motor speed.
+#define TWOWD_ENABLE_LEFT 5
 
-// The right motor is connected to pin 7.
-#define TWOWD_ENABLE_RIGHT 7
+// The PHASE pin determines the motor direction.
+#define TWOWD_PHASE_RIGHT 7
 
-// Pin 6 is the pulse-width modulation pin for the right motor.
-#define TWOWD_PULSE_RIGHT 6
+// The ENABLE pin can be supplied with a PWM signal to control the motor speed.
+#define TWOWD_ENABLE_RIGHT 6
 
 
 
@@ -105,10 +107,10 @@ bool TwoWD::begin()
    display.clearDisplay();
    display.display();
    
+   pinMode(TWOWD_PHASE_LEFT, OUTPUT);
    pinMode(TWOWD_ENABLE_LEFT, OUTPUT);
-   pinMode(TWOWD_PULSE_LEFT, OUTPUT);
+   pinMode(TWOWD_PHASE_RIGHT, OUTPUT);
    pinMode(TWOWD_ENABLE_RIGHT, OUTPUT);
-   pinMode(TWOWD_PULSE_RIGHT, OUTPUT);
 }
 
 bool TwoWD::begin(const char* ssid, const char* password, const char* hostname)
@@ -144,9 +146,12 @@ void TwoWD::driveForward()
   
    m_flags = TWOWD_FLAG_DRIVE_FORWARD;
   
-   digitalWrite(TWOWD_PULSE_LEFT, HIGH);
+   // Set both motors for forward movement.
+   digitalWrite(TWOWD_PHASE_LEFT, HIGH);
+   digitalWrite(TWOWD_PHASE_RIGHT, HIGH);
+   
+   // Start forward movement by writing the speed value onto the ENABLE pins.
    analogWrite(TWOWD_ENABLE_LEFT, m_speed);
-   digitalWrite(TWOWD_PULSE_RIGHT, HIGH);
    analogWrite(TWOWD_ENABLE_RIGHT, m_speed);
   
    m_isMoving = true;
@@ -160,7 +165,7 @@ void TwoWD::driveForward(uint16_t time)
        this->driveForward();
     }
 
-    if (((millis() - m_driveTimer) > time)) 
+    if (((millis() - m_driveTimer) > time) && (m_flags == TWOWD_FLAG_DRIVE_FORWARD)) 
     {
        this->stop();
        m_driveTimer = 0;
@@ -174,10 +179,13 @@ void TwoWD::driveBackward()
    display.display();
   
    m_flags = TWOWD_FLAG_DRIVE_BACKWARD;
-      
-   digitalWrite(TWOWD_PULSE_LEFT, LOW);
+
+   // Set both motors for backward movement.
+   digitalWrite(TWOWD_PHASE_LEFT, LOW);
+   digitalWrite(TWOWD_PHASE_RIGHT, LOW);
+
+   // Start backward movement by writing the speed value onto the ENABLE pins.
    analogWrite(TWOWD_ENABLE_LEFT, m_speed);
-   digitalWrite(TWOWD_PULSE_RIGHT, LOW);
    analogWrite(TWOWD_ENABLE_RIGHT, m_speed);
 
    m_isMoving = true;
@@ -191,7 +199,7 @@ void TwoWD::driveBackward(uint16_t time)
       this->driveBackward();
    }
 
-   if (((millis() - m_driveTimer) > time))
+   if (((millis() - m_driveTimer) > time) && (m_flags == TWOWD_FLAG_DRIVE_BACKWARD))
    {
       this->stop();
       m_driveTimer = 0;
@@ -200,6 +208,7 @@ void TwoWD::driveBackward(uint16_t time)
 
 void TwoWD::stop()
 {  
+   // Stop movement by writing zeros onto the ENABLE pins for both motors.
    analogWrite(TWOWD_ENABLE_LEFT, TWOWD_STOP_SPEED);
    analogWrite(TWOWD_ENABLE_RIGHT, TWOWD_STOP_SPEED);
 
